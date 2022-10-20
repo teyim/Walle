cfaced4bd6df3d3e09a3cd9642d10c7693793597
teyim <teyimasobo@gmail.com>
2022-10-20 18:37:15 +0100

    update ReadmeGen function to use dynamic image

--- ReadmeGen.js
+++ ReadmeGen.js
@@ -2,11 +2,8 @@ function generateReadme(health, level, commits) {
     return `
 <div style="text-align: center;" >
 
-![health](https://geps.dev/progress/${health})
+![walle image](https://facile-one.vercel.app/api/og?level=${level}&commits=${commits}&health=${health})
 
-<img src="./assets/${level}.svg" alt="walle image" style="height:150px" />
-
-### **LEVEL ${level}**
 </div>
     `
 }
